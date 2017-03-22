dabfd173ac08482c4f52ea8446d125c74972cedd
mrhos <erikhos@outlook.com>
2017-03-22 19:30:28 +0100

    updated feedback.html

--- js/form.js
+++ js/form.js
@@ -1,36 +1,33 @@
 $(function() {
     $('.error').hide();
+    
     $("#submit").click(function() {
-      // validate and process form here
-      
-      $('.error').hide();
-        var email = $("input#email").val();
-        if (email == "") {
-        $("label#email_error").show();
-        $("input#email").focus();
-        return false;
-      }
-        var message = $("#message").val();
-        if (message == "") {
-        $("label#message_error").show();
-        $("#message").focus();
-        return false;
-      }
-      
-      //alert (dataString);return false;
-      $.ajax({
-        url: "https://formspree.io/erikhos@outlook.com", 
-        method: "POST",
-        data: {message: "hello!"},
-        dataType: "json",
-        success: function() {
-          $('#form').html("<div id='message'></div>");
-          $('#message').html("<h2>Contact Form Submitted!</h2>")
-          .append("<p>We will be in touch soon.</p>")
-          .hide()
+        // validate form 
+        $('.error').hide();
+            var email = $("input#email").val();
+            if (email == "") {
+            $("label#email_error").show();
+            $("input#email").focus();
+            return false;
         }
-      });
-      return false;
+            var message = $("#message").val();
+            if (message == "") {
+            $("label#message_error").show();
+            $("#message").focus();
+            return false;
+        }
+
+        $.ajax({
+            url: "https://formspree.io/erikhos@outlook.com", 
+            method: "POST",
+            data: {message: "hello!"},
+            dataType: "json",
+            success: function() {
+                $('#form').html("<div id='message'></div>");
+                $('#message').html("<h2>Your message has been sent!</h2>").append("<p>Your feedback is very important to us!. Thank you for using MDNbot!</p>")
+            }
+        });
+        return false;
     });
 });
 
