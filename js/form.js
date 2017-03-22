3c81bb0e6a5c63906ecefebd338c946ea079a35b
mrhos <erikhos@outlook.com>
2017-03-22 19:36:26 +0100

    updated feedback.html

--- js/form.js
+++ js/form.js
@@ -17,17 +17,17 @@ $(function() {
             return false;
         }
 
-        $.ajax({
-            url: "https://formspree.io/erikhos@outlook.com", 
-            method: "POST",
-            data: {message: "hello!"},
-            dataType: "json",
-            success: function() {
-                $('#form').html("<div id='message'></div>");
-                $('#message').html("<h2>Your message has been sent!</h2>").append("<p>Your feedback is very important to us!. Thank you for using MDNbot!</p>")
-            }
-        });
-        return false;
+        // $.ajax({
+        //     url: "https://formspree.io/erikhos@outlook.com", 
+        //     method: "POST",
+        //     data: {message: "hello!"},
+        //     dataType: "json",
+        //     success: function() {
+        //         $('#form').html("<div id='message'></div>");
+        //         $('#message').html("<h2>Your message has been sent!</h2>").append("<p>Your feedback is very important to us!. Thank you for using MDNbot!</p>")
+        //     }
+        // });
+        // return false;
     });
 });
 
